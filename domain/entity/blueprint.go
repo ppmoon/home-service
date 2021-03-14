5ad6901b305f8e7b8f8b7974c94db6e77d8f9b89
ppmoon <272305610@qq.com>
2021-03-14 13:58:39 +0800

    add bolt db

--- domain/entity/blueprint.go
+++ domain/entity/blueprint.go
@@ -5,9 +5,9 @@ type Blueprint struct {
 	Version      string       `json:"version" yaml:"version"`
 	Category     string       `json:"category" yaml:"category"`
 	Image        string       `json:"image" yaml:"image"`
-	Ports        Ports        `json:"ports" yaml:"ports"`
-	Volumes      Volumes      `json:"volumes" yaml:"volumes"`
-	Environments Environments `json:"environments" yaml:"environments"`
+	Ports        []Ports        `json:"ports" yaml:"ports"`
+	Volumes      []Volumes      `json:"volumes" yaml:"volumes"`
+	Environments []Environments `json:"environments" yaml:"environments"`
 	Dependence   []Dependence `json:"dependence" yaml:"dependence"`
 }
 
