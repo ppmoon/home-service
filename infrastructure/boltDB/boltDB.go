dc970b238bffcc53dd728c6de3d02a07ba9e789a
ppmoon <272305610@qq.com>
2021-03-17 22:15:40 +0800

    add getBoltDB function

--- infrastructure/boltDB/boltDB.go
+++ infrastructure/boltDB/boltDB.go
@@ -1,2 +1,13 @@
-package boltDB
+package bolt
 
+import bolt "go.etcd.io/bbolt"
+
+func GetBoltDB(dbPath string) (*bolt.DB, error) {
+	db, err := bolt.Open(dbPath, 0600, nil)
+	if err != nil {
+		return nil, err
+	}
+	//
+	defer db.Close()
+	return db, nil
+}
