f5832f55eff137c5abb4e1262d8e9c4d544bb2cb
ppmoon <272305610@qq.com>
2022-01-14 09:25:21 +0800

    delete rate of process

--- domain/app/entity/pipeline.go
+++ domain/app/entity/pipeline.go
@@ -1,10 +1,9 @@
 package entity
 
 type Pipeline struct {
-	Name          string
-	StepList      []PipelineStep
-	Cursor        int
-	RateOfProcess int
+	Name     string
+	StepList []PipelineStep
+	Cursor   int
 }
 
 func NewPipeline(name string) *Pipeline {
